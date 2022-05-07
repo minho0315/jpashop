3ab2666a2fa28679636e072f43b72b598accc3e8
minho0315 <dlalsgh369@naver.com>
2022-05-07 20:04:21 +0900

    Initial commit

---
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
