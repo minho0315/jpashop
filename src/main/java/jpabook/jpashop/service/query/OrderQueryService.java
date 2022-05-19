b113832659afa0b3816748a6c202e1261663b6b5
minho0315 <dlalsgh369@naver.com>
2022-05-20 00:02:21 +0900

    OSIV와 성능 최적화

---
+++ src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
@@ -0,0 +1,8 @@
+package jpabook.jpashop.service.query;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public class OrderQueryService {
+    // 로직을 여기에 넣는다.
+}
